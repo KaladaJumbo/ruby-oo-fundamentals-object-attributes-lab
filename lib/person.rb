feea1e791bec5c3b33c308fea8d8fe71e2af3550
KaladaJumbo <wkalada1@gmail.com>
2020-08-25 14:34:52 -0400

    final commit

---
+++ lib/person.rb
@@ -0,0 +1,30 @@
+class Person 
+
+    @name = ""
+    @job = ""
+
+    def name=(name)
+
+        @name = name
+
+    end
+
+    def name 
+
+        @name
+
+    end
+
+    def job=(job_title)
+
+        @job = job_title
+
+    end
+
+    def job 
+
+        @job
+
+    end
+
+end
